a4289ec7d369983651dcba90b8953a89af9bb0b1
jorgeDonoso <jrg.donoso@gmail.com>
2019-11-03 23:17:58 -0300

    primer commit

--- src/src/main/java/net/itinajero/app/repository/DetalleRepository.java
+++
@@ -1,9 +0,0 @@
-package net.itinajero.app.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import net.itinajero.app.model.Detalle;
-
-public interface DetalleRepository extends JpaRepository<Detalle, Integer> {
-
-}
--- src/src/main/java/net/itinajero/app/service/INoticiaService.java
+++
@@ -1,9 +0,0 @@
-package net.itinajero.app.service;
-
-import net.itinajero.app.model.Noticia;
-
-public interface INoticiaService {
-	
-	void guardar(Noticia noticia);
-
-}
